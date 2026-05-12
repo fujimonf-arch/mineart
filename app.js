b6f36dce12993dd92ee13ed78ebe937859071f7d
fujimonf-arch <fujimonf@gmail.com>
2026-05-12 21:22:53 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-async function loadPuzzle() {
-
-  const res = await fetch("./puzzles/cat.json");
-
-  const puzzle = await res.json();
-
-  console.log(puzzle);
-
-}
-
-loadPuzzle();
+console.log("MineArt JS OK");
