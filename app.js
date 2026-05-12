f1c9ab4bfd9793577594119364229265021876a4
fujimonf-arch <fujimonf@gmail.com>
2026-05-12 21:38:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,37 @@ async function loadPuzzle() {
 
   const res = await fetch("./puzzles/cat.json");
 
-  const data = await res.json();
+  const puzzle = await res.json();
 
-  console.log(data);
+  drawBoard(puzzle);
+
+}
+
+function drawBoard(puzzle) {
+
+  const game = document.getElementById("game");
+
+  for (let y = 0; y < puzzle.height; y++) {
+
+    const row = document.createElement("div");
+    row.className = "row";
+
+    for (let x = 0; x < puzzle.width; x++) {
+
+      const cell = document.createElement("div");
+      cell.className = "cell";
+
+      if (puzzle.board[y][x] === 1) {
+        cell.style.background = "white";
+      }
+
+      row.appendChild(cell);
+
+    }
+
+    game.appendChild(row);
+
+  }
 
 }
 
