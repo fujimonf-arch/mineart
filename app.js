720e9d0364b1a83edc8e85f400c9bfd8dd3e74a5
fujimonf-arch <fujimonf@gmail.com>
2026-05-12 22:50:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 async function loadPuzzle() {
 
-  const res = await fetch("./puzzles/cat.json");
+  const params = new URLSearchParams(location.search);
+
+  const puzzleName = params.get("p") || "cat";
+
+  const res = await fetch(`./puzzles/${puzzleName}.json`);
 
   const puzzle = await res.json();
 
@@ -14,18 +18,24 @@ function drawBoard(puzzle) {
 
   const game = document.getElementById("game");
 
+  game.innerHTML = "";
+
   for (let y = 0; y < puzzle.height; y++) {
 
     const row = document.createElement("div");
+
     row.className = "row";
 
     for (let x = 0; x < puzzle.width; x++) {
 
       const cell = document.createElement("div");
+
       cell.className = "cell";
 
       if (puzzle.board[y][x] === 1) {
+
         cell.style.background = "white";
+
       }
 
       row.appendChild(cell);
