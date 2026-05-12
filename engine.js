86a0e3c84b10b52927090c952cafe2ddb74dee56
fujimonf-arch <fujimonf@gmail.com>
2026-05-12 22:33:50 +0900

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,1 +1,28 @@
+function inside(x,y,H,W){
 
+    return (
+        x>=0 &&
+        x<H &&
+        y>=0 &&
+        y<W
+    );
+}
+
+function getArea(i,j,H,W){
+
+    const arr=[];
+
+    for(let dx=-1;dx<=1;dx++){
+        for(let dy=-1;dy<=1;dy++){
+
+            const x=i+dx;
+            const y=j+dy;
+
+            if(inside(x,y,H,W)){
+                arr.push([x,y]);
+            }
+        }
+    }
+
+    return arr;
+}
